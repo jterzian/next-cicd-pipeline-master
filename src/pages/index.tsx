78e26e487445d0f70c681ef7bb41dfb1997e2d43
jterzian <jterzian@construtivo.com>
2026-02-26 21:04:00 -0300

    feat: setup completo de lint e pagina inicial

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <div style={{ 
+      fontFamily: 'sans-serif', 
+      display: 'flex', 
+      flexDirection: 'column', 
+      alignItems: 'center', 
+      justifyContent: 'center', 
+      height: '100vh' 
+    }}>
+      <h1>🚀 Pipeline CI/CD com Sucesso!</h1>
+      <p>Projeto de Front-end automatizado com GitHub Actions.</p>
+      <p>Desenvolvido por: João Terzian</p>
+    </div>
+  )
+}
